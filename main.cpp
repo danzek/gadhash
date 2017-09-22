b1f2e795000ff45705744584c7585b60afcf04fd
Dan <d@4n68r.com>
2017-09-22 02:55:33 -0500

    minor comment tweak

--- main.cpp
+++ main.cpp
@@ -158,7 +158,7 @@ int main(int argc, const char* argv[]) {
         // boost literal-escapes all user input (which is secure, but in this case I want a tab char)
         if (delimiter == "\\t") {
             delimiter = "\t";
-        } else if (delimiter == "\\n") {  // I suppose someone could want newline delimiter for some reason (consider)
+        } else if (delimiter == "\\n") {  // I suppose someone could want a newline delimiter for some reason (consider)
             delimiter = "\n";
         }
 
